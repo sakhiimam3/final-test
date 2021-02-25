1322f52cce2cf1c59adfb6ccf2ce0137810b3fb5
sakhiimam3 <sakhiimam3411@gmail.com>
2021-02-25 23:56:43 +0500

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
+import {useState} from "react"
+import React from 'react';
+import firebase from "./src/components/config/firebase"
+import { StyleSheet, Text, View ,TextInput, ImageBackground ,Button, ScrollView ,TouchableOpacity} from 'react-native';
 export default function App() {
+  const [gettodo,setTodo]=useState("")
+  const [alltodo,setallTodo]=useState([])
+  
+
+
+
+
+  
+  let obj={
+          alltodo,
+  }
+  
+  firebase.database().ref("todos/").set(obj)
+ const AddItem=()=>{
+           setallTodo([...alltodo,{key:Math.random().toString(), data: gettodo}])
+           setTodo("");
+ }
+
+
+ const remove = (itemkey)=>{
+    let list=alltodo.filter(item=>item.key != itemkey)
+    setallTodo(list)
+  firebase.database().ref("todos/"+ itemkey.key).remove()
+    
+ }
+
+ const editTodo = () => {
+     
+    
+  
+}
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    
+    <ImageBackground   source={require('./assets/back.jpg')}           style={styles.container}>
+        <Text style={styles.Title}> TODO APP WITH  DATABASE </Text>
+       <View style={styles.InputContainer}>
+          <TextInput  value={gettodo}  onChangeText={(text)=>setTodo(text)}   placeholderTextColor = "#fff"  style={styles.input}  placeholder="Enter todo" /> 
+        <Button    onPress={AddItem} title="Add" /> 
     </View>
+      
+            <ScrollView style={styles.scrollar}>
+             {alltodo.map((item)=>
+             
+               <View style={styles.ScrollViewItem}>
+             <Text style={styles.itemtext}>{item.data}</Text> 
+             <View>
+             </View>
+
+
+             <TouchableOpacity style={styles.crossText}>
+                         <Text onPress={()=>remove(item.key)}  key={item.key}  TouchableOpacity={0.7} style={styles.delBtn}> X</Text> 
+                         </TouchableOpacity>
+                    <TouchableOpacity>   
+                      <Text  onPress={()=>editTodo(item.key)} style={styles.editBtn}> Edit</Text>
+                  </TouchableOpacity>
+
+             </View> 
+                    )}
+               </ScrollView>
+    </ImageBackground>
+  
   );
 }
 
@@ -17,5 +75,104 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop:25,
+    marginTop:20,
   },
+
+
+  Title:{
+            fontSize:20,
+            color:"purple",
+            fontFamily:"sans-serif",
+            backgroundColor:"purple",
+            color:"white",
+            borderRadius:10,
+            padding:10,
+            shadowColor: "#000",
+shadowOffset: {
+	width: 0,
+	height: 12,
+},
+shadowOpacity: 0.58,
+shadowRadius: 16.00,
+
+elevation: 24,
+
+          
+          
+
+  },
+
+  InputContainer:{
+    marginTop:20,
+        flexDirection:"row",
+        width:"70%",
+        justifyContent:"space-between",
+        alignItems:'center'
+           
+
+        
+  },
+
+
+  input:{
+
+                    borderColor:"red",
+                    width:"80%",
+                    fontSize:16,
+                    padding:10,
+                    borderBottomWidth:2,
+                    color:'white'
+
+             
+  },
+  
+  scrollar:{
+       
+          width:"90%",
+          marginTop:30,
+          
+  },
+  ScrollViewItem:{
+    flexDirection:"row",
+    justifyContent:"space-between",
+    width:"90%",
+    alignSelf:"center",
+    padding:10,
+    margin:5,
+    backgroundColor:"#BEDC88",
+    borderRadius:25,
+
+  },
+
+  itemtext:{
+         color:"gray",
+           fontWeight:'400',
+           fontSize:23,
+  },
+  
+
+  crossText:{
+          
+          
+          fontSize:18,
+         
+          
+          
+  },
+
+
+  delBtn:{
+            color:"red",
+            marginLeft:160,
+
+  },
+
+
+  editBtn:{ 
+             color:"green"  
+
+  }
+
+
 });
---
+++ src/components/config/firebase.js
@@ -0,0 +1,13 @@
+
+
+import firebase from "firebase"
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBLdXG7Yzijk_mihHT93kChD60b7_4Kn_I",
+    authDomain: "final-test-95f08.firebaseapp.com",
+    projectId: "final-test-95f08",
+    storageBucket: "final-test-95f08.appspot.com",
+    messagingSenderId: "242194166863",
+    appId: "1:242194166863:web:ceabf5689fbd89e160e63a"
+  };
+ export default firebase.initializeApp(firebaseConfig);
